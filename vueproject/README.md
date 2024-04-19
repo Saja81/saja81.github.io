035454a96c858d32ac267952b1e8dd6e712769f3
Sandr <sandrajakobsson81@gmail.com>
2024-04-19 15:02:29 +0200

    Uploading Vue project

---
+++ vueproject/README.md
@@ -0,0 +1,5 @@
+# A Vue.js template for Vite
+# vite_project_1
+# vite_project_1
+# vite_project_2
+# vite_project_2
---
+++ vueproject/src/components/PropsComponent.vue
@@ -0,0 +1,34 @@
+<script>
+  export default {
+    data() {
+      return {
+        name: this.test
+      };
+    },
+
+    props: {
+      test: {
+        type: String,
+        default: "test"
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  $some-color: green;
+  $some-color2: blue;
+  p {
+    color: $some-color;
+  }
+  div {
+    color: $some-color2;
+  }
+</style>
+
+<!-- function (name) { console.log(name) } -->
+
+<template>
+  <p>Här har jag skapat en komponent med props</p>
+  <div>Hej {{ test }}!</div>
+</template>
